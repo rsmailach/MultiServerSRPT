2a0f9c2d36748d6a1e555e045a22c06da79e5756
Rachel Mailach <rachel.mailach@gmail.com>
2015-09-21 14:53:50 -0400

    Cleaned up distribution setting

--- ApproxSRPTE.py
+++ ApproxSRPTE.py
@@ -150,17 +150,16 @@ class GUI(Tk):
 		self.clearSavedArrivals()
 		I = Input(self)     
 
-		self.printParams(I.valuesList[0], I.valuesList[1],\
-						 I.valuesList[2], I.valuesList[3],\
-						 I.valuesList[4])
+		self.printParams(I.valuesList[0], I.valuesList[1], I.valuesList[2], I.valuesList[3], I.valuesList[4])
 
 		main.timesClicked = 0
 		
 		# Start process
 		MC = MachineClass(self)
-		MC.run(I.valuesList[0], 'Poisson',\
+		MC.run(	I.valuesList[0], I.distList[0],\
 				I.valuesList[1], I.distList[1],\
-				I.valuesList[2], I.valuesList[3],\
+				I.valuesList[2],\
+				I.valuesList[3],\
 				I.valuesList[4])
 
 		self.displayAverageData()
@@ -219,12 +218,12 @@ class Input(LabelFrame):
 
 
 		# Distribution Dropdowns
-		self.distributions = ('Select Distribution', 'Exponential', 'Uniform', 'Custom')
-		#self.comboBox_1 = ttk.Combobox(self, values = self.distributions, state = 'readonly')
-		#self.comboBox_1.current(0) # set selection
-		#self.comboBox_1.grid(row = 0, column = 2)
+		self.distributions = ('Select Distribution', 'Poisson', 'Exponential', 'Uniform', 'Custom')
+		self.comboBox_1 = ttk.Combobox(self, values = self.distributions, state = 'disabled')
+		self.comboBox_1.current(1) # set selection
+		self.comboBox_1.grid(row = 0, column = 3)
 		self.comboBox_2 = ttk.Combobox(self, values = self.distributions, state = 'readonly')
-		self.comboBox_2.current(1) # set default selection                  #####################CHANGE LATER
+		self.comboBox_2.current(2) # set default selection                  #####################CHANGE LATER
 		self.comboBox_2.grid(row = 1, column = 3)
 
 		# Simulate Button
@@ -266,14 +265,14 @@ class Input(LabelFrame):
 				return 0
 
 	def getDropDownValues(self):
-		#if self.comboBox_1.get() == 'Select Distribution': print "Box 1 has to have a selection"
+		comboBox1Value = self.comboBox_1.get()
 		comboBox2Value = self.comboBox_2.get()
 		if comboBox2Value == 'Select Distribution':
 				self.errorMessage.set("You must select a distribution for the processing rate")
 				return 1
 		else:
 				self.errorMessage.set("")
-				Input.distList = ["", comboBox2Value, "", "", "", ""]
+				Input.distList = [comboBox1Value, comboBox2Value]
 				return 0
 
 #----------------------------------------------------------------------#
@@ -465,19 +464,13 @@ class JobClass(object):
 
 	# Dictionary of service distributions
 	def setServiceDist(self, procRate, procDist):
-		self.ServiceDistributions =  {
-			'Exponential': self.setExponDist,
-			'Uniform': self.setUniformDist,
-			#'Normal': Rnd.normalvariate(self.ServiceRate)
+		ServiceDistributions =  {
+			'Poisson': random.expovariate(1.0/procRate),
+			'Exponential': random.expovariate(procRate),
+			'Uniform': random.uniform(0.0, procRate),
 			'Custom': self.setCustomDist
 		}
-		return self.ServiceDistributions[procDist](procRate)
-
-	def setExponDist(self, procRate):
-		return random.expovariate(procRate)
-
-	def setUniformDist(self, procRate):
-		return random.uniform(0.0, procRate)
+		return ServiceDistributions[procDist]
 
 	def setCustomDist(self, procRate):
 		if main.timesClicked == 0:
